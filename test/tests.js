396e1eccf5adac1d948bdce630be88220d852db7
Luis Herranz <luisherranz@gmail.com>
2016-08-29 17:26:52 +0200

    Return individual reducers so we are able to load and unload them.

--- test/tests.js
+++ test/tests.js
@@ -29,18 +29,18 @@ test('Get reducers', function(t) {
   var pkg1 = { reducers: { someRed: 11, default: () => 1 }};
   var pkg2 = { reducers: { someRed: 22, default: () => 2 }};
   worona.addPackage('pkg1', pkg1);
-  t.deepEqual(worona.getReducers(), { pkg1: 1 });
+  t.is(worona.getReducers('pkg1'), 1);
   worona.addPackage('pkg2', pkg2);
-  t.deepEqual(worona.getReducers(), { pkg1: 1, pkg2: 2 });
+  t.is(worona.getReducers('pkg2'), 2);
 });
 
 test('Get reducers. Packages without reducers', function(t) {
   var pkg1 = { reducers: { someRed: 11, default: () => 1 }};
   var pkg2 = {};
   worona.addPackage('pkg1', pkg1);
-  t.deepEqual(worona.getReducers(), { pkg1: 1 });
+  t.is(worona.getReducers('pkg1'), 1);
   worona.addPackage('pkg2', pkg2);
-  t.deepEqual(worona.getReducers(), { pkg1: 1 });
+  t.is(worona.getReducers('pkg2'), null);
 });
 
 test('Get locales. Empty locales', function(t) {
